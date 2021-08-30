2716ae65c5c0afb7d39af7244917848642635638
merlin <clamores.pro@gmail.com>
2021-08-30 22:03:30 +0300

    slice-2: export functions and types

--- slice.go
+++ slice.go
@@ -2,23 +2,23 @@ package golds
 
 // Slice is a sequence of values.
 // Basically it's an utility wrapper around a plain slice.
-type slice[E any] []E
+type Slice[E any] []E
 
 // NewSlice puts provided values into resulting slice.
 //export
-func newSlice[E any] (vv ... E) slice[E] {
-	return slice[E](vv)
+func NewSlice[E any] (vv ... E) Slice[E] {
+	return Slice[E](vv)
 }
 
 // Repeat returns a new Slice[E] with n copies of v.
-func repeat[E any](n int, v E) slice[E] {
+func Repeat[E any](n int, v E) Slice[E] {
 	switch{
 	case n == 0:
 		return nil
 	case n < 0:
 		panic("golds.Repeat: negative Repeat count")
 	}
-	var s = make(slice[E], n)
+	var s = make(Slice[E], n)
 	// TODO: use copy to fill slice for better performance
 	for i := range s {
 		s[i] = v
@@ -27,14 +27,14 @@ func repeat[E any](n int, v E) slice[E] {
 } 
 
 // SliceIterFn returns a new Slice[E] with results of fn(i) for i := range [0, n].
-func sliceIterFn[E any](n int, fn func(int) E) slice[E] {
+func SliceIterFn[E any](n int, fn func(int) E) Slice[E] {
 	switch{
 	case n == 0:
 		return nil
 	case n < 0:
 		panic("golds.Repeat: negative Repeat count")
 	}
-	var s = make(slice[E], n)
+	var s = make(Slice[E], n)
 	for i := range s {
 		s[i] = fn(i)
 	}
@@ -42,15 +42,15 @@ func sliceIterFn[E any](n int, fn func(int) E) slice[E] {
 }
 
 // Len returns the number of elements in the slice.
-func(s slice[E]) Len() int { return len(s) }
+func(s Slice[E]) Len() int { return len(s) }
 
 // Cap returns capacity of underlying slice.
-func(s slice[E]) Cap() int { return cap(s) }
+func(s Slice[E]) Cap() int { return cap(s) }
 
 // Index returns element by the index.
 // If index is negative, then returns element len-index.
 // Panics if index is out of [-len, len] range.
-func(s slice[E]) Index(i int) E {
+func(s Slice[E]) Index(i int) E {
 	if i < 0 {
 		return s[s.Len() - i]
 	}
@@ -59,7 +59,7 @@ func(s slice[E]) Index(i int) E {
 
 // Swap two elements in the slice.
 // If i or j are negative, then uses elements len-i(j).
-func(s slice[E]) Swap(i, j int) { 
+func(s Slice[E]) Swap(i, j int) { 
 	if i < 0 {
 		i = s.Len() - i
 	}
@@ -70,7 +70,7 @@ func(s slice[E]) Swap(i, j int) {
 }
 
 // Count returns the number of elements e, where fn(e) is true.
-func(s slice[E]) Count(fn func(v E) bool) int {
+func(s Slice[E]) Count(fn func(v E) bool) int {
 	var i int
 	for _, v := range s {
 		if fn(v) {
@@ -82,8 +82,8 @@ func(s slice[E]) Count(fn func(v E) bool) int {
 
 // Filter returns slice of elements e, where fn(e) is true.
 // fn can be called multiple times for each element.
-func(s slice[E]) Filter(fn func(v E) bool) slice[E] {
-	var filtered = make(slice[E], 0, s.Count(fn))
+func(s Slice[E]) Filter(fn func(v E) bool) Slice[E] {
+	var filtered = make(Slice[E], 0, s.Count(fn))
 	for _, v := range s {
 		if fn(v) {
 			filtered = append(filtered, v)
@@ -92,7 +92,7 @@ func(s slice[E]) Filter(fn func(v E) bool) slice[E] {
 	return filtered
 }
 
-func(s slice[E]) FilterInPlace(fn func(v E) bool) {
+func(s Slice[E]) FilterInPlace(fn func(v E) bool) {
 	var i int
 	for _, v := range s {
 		if fn(v) {
@@ -108,8 +108,8 @@ func(s slice[E]) FilterInPlace(fn func(v E) bool) {
 // If index is negative, then len-index element will be used.
 // Example:
 //	Slice[int]{1, 2, 3}.Select(-1, 0, 2) -> Slice[int]{3, 1, 2}
-func(s slice[E]) Select(indexes ... int) slice[E] {
-	var selected = make(slice[E], 0, len(indexes))
+func(s Slice[E]) Select(indexes ... int) Slice[E] {
+	var selected = make(Slice[E], 0, len(indexes))
 	for _, i := range indexes {
 		selected = append(selected, s.Index(i))
 	}
@@ -117,8 +117,8 @@ func(s slice[E]) Select(indexes ... int) slice[E] {
 }
 
 // Apply creates a new slice with mapped values.
-func(s slice[E]) Apply(fn func(v E) E) slice[E] {
-	var result = make(slice[E], 0, s.Len())
+func(s Slice[E]) Apply(fn func(v E) E) Slice[E] {
+	var result = make(Slice[E], 0, s.Len())
 	for _, v := range s {
 		result = append(result, fn(v))
 	}
@@ -126,14 +126,14 @@ func(s slice[E]) Apply(fn func(v E) E) slice[E] {
 } 
 
 // Append new elements to the slice in place.
-func(s *slice[E]) Append(vv ...E) {
+func(s *Slice[E]) Append(vv ...E) {
 	*s = append(*s, vv...)
 }
 
 
 // Pop returns returns the last element and removes it from the slice.
 // If the slice is empty, then returns false.
-func(s *slice[E]) Pop() (E, bool) {
+func(s *Slice[E]) Pop() (E, bool) {
 	var empty E
 	var n = s.Len()
 	if n == 0 {
@@ -145,14 +145,14 @@ func(s *slice[E]) Pop() (E, bool) {
 	return v, true
 }
 
-func(s slice[E]) Copy() slice[E] {
-	var cp = make(slice[E], s.Len())
+func(s Slice[E]) Copy() Slice[E] {
+	var cp = make(Slice[E], s.Len())
 	copy(cp, s)
 	return cp
 }
 
-func(s slice[E]) CopyWith(fn func(E) E) slice[E] {
-	var cp = make(slice[E], s.Len())
+func(s Slice[E]) CopyWith(fn func(E) E) Slice[E] {
+	var cp = make(Slice[E], s.Len())
 	for i, v := range s {
 		cp[i] = fn(v)
 	}
@@ -160,7 +160,7 @@ func(s slice[E]) CopyWith(fn func(E) E) slice[E] {
 }
 
 // Fill slice using provided value.
-func(s slice[E]) Fill(v E) {
+func(s Slice[E]) Fill(v E) {
 	var n = s.Len()
 	if n == 0 {
 		return
@@ -172,7 +172,7 @@ func(s slice[E]) Fill(v E) {
 }
 
 // FillWith uses results of fn to fill the slice.
-func(s slice[E]) FillWith(fn func() E) {
+func(s Slice[E]) FillWith(fn func() E) {
 	for i := range s {
 		s[i] = fn()
 	}
@@ -181,7 +181,7 @@ func(s slice[E]) FillWith(fn func() E) {
 // Insert value at i-position, shifting elements to the end of slice.
 // Panics if the index is out of range.
 // 	Slice{1, 2, 3}.Insert(1, 100) -> Slice{1, 100, 2, 3}
-func(s *slice[E]) Insert(i int, v E) {
+func(s *Slice[E]) Insert(i int, v E) {
 	var sl = *s
 	*s = append(sl[:i+1], sl[i:]...)
 	(*s)[i] = v
@@ -190,7 +190,7 @@ func(s *slice[E]) Insert(i int, v E) {
 // Delete value at i-position, shifting elements to the begining of slice.
 // Panics if the index is out of range.
 // 	Slice{1, 2, 3}.Delete(1) -> Slice{2, 3}
-func(s *slice[E]) Delete(i int) {
+func(s *Slice[E]) Delete(i int) {
 	var sl = *s
 	sl = append(sl[:i], sl[i+1:]...)
 
@@ -201,7 +201,7 @@ func(s *slice[E]) Delete(i int) {
 
 // DeleteNoOrder exchanges the i-th and the last element 
 // of the slice and cuts the last, now duplicated, element.
-func(s *slice[E]) DeleteNoOrder(i int) {
+func(s *Slice[E]) DeleteNoOrder(i int) {
 	var n = s.Len()
 	var sl = *s
 	sl[i] = sl[n-1]
